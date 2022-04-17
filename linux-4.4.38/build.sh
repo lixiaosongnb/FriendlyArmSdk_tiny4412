45b31536c8470fd9d9927bd8ceb59bcc91ead94a
xiaosong.li <2456102575@qq.com>
2022-04-17 22:37:35 +0800

    [Optimize] optimize the build.sh and delete some file

--- linux-4.4.38/build.sh
+++ linux-4.4.38/build.sh
@@ -1,4 +1,36 @@
-mkimage -A arm -O linux -T kernel -C none -a 0x40008000 -e 40008040 -n linux-4.4 -d /home/lxs/FriendlyArm-Tiny4412-SDK/linux-4.4.38/arch/arm/boot/zImage uImage
+# This bash aim to easy make uImage and dtb copy to this directory flash
+#!/bin/bash
+
+LINUX_KERNEL_VERSION=linux-4.4
+
+ARCH=arm
+OS=linux
+KERNEL_LOAD_ARRT=40008000
+KERNEL_START_ADDR=40008040
+
+ZIMAGE_PWD=$PWD/arch/arm/boot
+
+DTB_PWD=$PWD/arch/arm/boot/dts
+DTB_FILE_NAME=exynos4412-tiny4412.dtb
+
+UIMAGE_STORD_PWD=$pwd
+
+echo "Degin make uImage and dtb..."
+
+mkimage -A $ARCH -O $OS -T kernel -C none -a $KERNEL_LOAD_ARRT -e $KERNEL_START_ADDR -n $LINUX_KERNEL_VERSION -d $ZIMAGE_PWD/zImage uImage
+
+if [ $? -ne 0 ];then
+	echo "mkimage failed..exit"
+	exit
+fi
 
 make dtbs
-cp ./arch/arm/boot/dts/exynos4412-tiny4412.dtb  ./
+
+if [ $? -ne 0 ];then
+	echo "make dtb failed..exit"
+	exit
+fi
+
+cp $DTB_PWD/$DTB_FILE_NAME ./
+
+echo "Done"
